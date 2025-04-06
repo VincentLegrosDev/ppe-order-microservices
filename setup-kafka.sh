98c12e5ffdc0bc5f67c45b659497f7dfcc6248cc
Vincent Legros <vincent.legros22@gmail.com>
2025-04-06 01:31:20 -0400

    deliverable kafka basic using command line

--- setup-kafka.sh
+++ setup-kafka.sh
@@ -1,19 +1,16 @@
 docker compose -f bitnami-kafka.yml up -d
 
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --topic Order-received
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --describe --topic Order-received
+docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create  --replication-factor 1 --partitions 1 --config retention.ms=10800000 --topic OrderReceived 
+docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --describe --topic OrderReceived
 
-docker exec -it kafka kafka-configs.sh --bootstrap-server kafka:9092 --entity-type topics --entity-name  Order-received  --alter --add-config retention.ms=259200000
+docker exec -it kafka kafka-configs.sh --bootstrap-server kafka:9092 --entity-type topics --entity-name  OrderReceived  --alter --add-config retention.ms=259200000
 
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --topic Order-confirmed
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --describe --topic Order-confirmed
+docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --replication-factor 1 --partitions 1 --config retention.ms=10800000  --topic OrderConfirmed
+docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --describe --topic OrderConfirmed
 
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --topic Order-picked 
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --describe --topic Order-picked
+docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --replication-factor 1 --partitions 1 --config retention.ms=10800000 --topic OrderPickedAndPacked
+docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --describe --topic OrderPickedAndPacked
 
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --topic Order-packed 
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --describe --topic Order-packed
-
-docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --topic Notifications
+docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --replication-factor 1 --partitions 1 --config retention.ms=10800000 --topic Notifications
 docker exec -it kafka kafka-topics.sh --bootstrap-server kafka:9092 --describe --topic Notifications
 
