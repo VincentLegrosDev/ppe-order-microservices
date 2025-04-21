7a96cf3f9065cdeee0ae0eae75dd52cf98e03042
Vincent Legros <vincent.legros22@gmail.com>
2025-04-20 23:56:51 -0400

    deliverable order servce and first event

--- order-api/models/order.go
+++ order-api/models/order.go
@@ -5,27 +5,26 @@ import (
 )
 
 type Product struct {
-	Quantity  uint32    `json:"quantity"`
-	ProductId uuid.UUID `json:"productId"`
+	Quantity    uint32 `json:"quantity" binding:"required"`
+	ProductCode string `json:"productCode" binding:"required"`
 }
 
 type Customer struct {
-	FirstName       string          `json:"firstName"`
-	LastName        string          `json:"lastName"`
-	EmailAddresse   string          `json:"emailAddresse"`
-	ShippingAddress ShippingAddress `json:"shippingAddress"`
+	FirstName       string          `json:"firstName" binding:"required"`
+	LastName        string          `json:"lastName" binding:"required"`
+	EmailAddress    string          `json:"emailAddress" binding:"required,email"`
+	ShippingAddress ShippingAddress `json:"shippingAddress" binding:"required"`
 }
 
 type ShippingAddress struct {
-	Line1      string `json:"line1"`
-	City       string `json:"city"`
-	State      string `json:"state"`
-	PostalCode string `json:"postalCode"`
+	Line1      string `json:"line1" binding:"required"`
+	City       string `json:"city" binding:"required"`
+	State      string `json:"state" binding:"required"`
+	PostalCode string `json:"postalCode" binding:"required"`
 }
 
 type Order struct {
-	OrderId         uuid.UUID       `json:"orderId"`
-	Products        []Product       `json:"products"`
-	Customer        Customer        `json:"customer"`
-	ShippingAddress ShippingAddress `json:"shippingAddress"`
+	OrderId  uuid.UUID `json:"orderId" binding:"required"`
+	Products []Product `json:"products" binding:"required"`
+	Customer Customer  `json:"customer" binding:"required"`
 }
